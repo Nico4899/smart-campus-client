78549c294dfb77836fc60ec8b49ae0a39859e5d8
Felix <uqdgd@student.kit.edu>
2022-09-04 21:30:59 +0200

    Add actual Dialog window "Add room"

--- src/app/shared/dialogs/add-room/add-room.component.ts
+++ src/app/shared/dialogs/add-room/add-room.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 
+import {GrpcRoomType} from '../../../../proto/generated/building_management_pb'
+
+import {MatDialogRef} from "@angular/material/dialog";
+import {FormControl, Validators} from "@angular/forms";
+
 @Component({
   selector: 'app-add-room',
   templateUrl: './add-room.component.html',
   styleUrls: ['./add-room.component.css']
 })
-export class AddRoomComponent implements OnInit {
+export class AddRoomComponent {
 
-  constructor() { }
+  floor !: number;
 
-  ngOnInit(): void {
+  roomName!: string;
+  roomNumber!: string;
+
+  roomType!: GrpcRoomType;
+
+  roomTypes = Object.values(GrpcRoomType);
+
+  formControl = new FormControl('', [Validators.required]);
+
+  constructor(public dialogRef: MatDialogRef<AddRoomComponent>) {
+    this.dialogRef.disableClose = true;
+    this.roomTypes = this.roomTypes.filter(e => e != 0);
   }
+
+  ok() {
+    this.dialogRef.close({ event: 'ok',
+      data: {
+        roomName: this.roomName,
+        roomNumber: this.roomNumber,
+        roomType: this.roomType,
+        floor: this.floor
+      }
+
+    });
+
+  }
+
+  cancel() {
+    this.dialogRef.close({event: 'cancel'});
+  }
+
 
 }
