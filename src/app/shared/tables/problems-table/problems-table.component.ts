e4cbbfbb56874317dbd6a069a3e10007367edb91
Nico <ubncr@student.kit.edu>
2022-09-04 14:36:02 +0200

    add problem table

--- src/app/shared/tables/problems-table/problems-table.component.ts
+++ src/app/shared/tables/problems-table/problems-table.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+import {BuildingManagementConnectorService} from "../../../core/connectors/building-management-connector.service";
+import {MatDialog} from "@angular/material/dialog";
+import {ProblemManagementConnectorService} from "../../../core/connectors/problem-management-connector.service";
+
+export interface UserData {
+  id: string;
+  name: string;
+  progress: string;
+  fruit: string;
+}
+
+/** Constants used to fill up our data base. */
+const FRUITS: string[] = [
+  'blueberry',
+  'lychee',
+  'kiwi',
+];
+const NAMES: string[] = [
+  'Maia',
+  'Asher',
+  'Olivia',
+];
+
 
 @Component({
   selector: 'app-problems-table',
   templateUrl: './problems-table.component.html',
   styleUrls: ['./problems-table.component.css']
 })
-export class ProblemsTableComponent implements OnInit {
+export class ProblemsTableComponent implements AfterViewInit {
+  displayedColumns: string[] = ['id', 'name', 'progress', 'fruit'];
+  dataSource: MatTableDataSource<UserData>;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit(): void {
+  constructor(private problemManagementConnector: ProblemManagementConnectorService, private dialog: MatDialog) {
+    // Create 100 users
+    const users = Array.from({length: 100}, (_, k) => createNewUser(k + 1));
+
+    // Assign the data to the data source for the table to render
+    this.dataSource = new MatTableDataSource(users);
   }
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+}
+
+/** Builds and returns a new User. */
+function createNewUser(id: number): UserData {
+  const name =
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))] +
+    ' ' +
+    NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) +
+    '.';
+
+  return {
+    id: id.toString(),
+    name: name,
+    progress: Math.round(Math.random() * 100).toString(),
+    fruit: FRUITS[Math.round(Math.random() * (FRUITS.length - 1))],
+  };
 }
