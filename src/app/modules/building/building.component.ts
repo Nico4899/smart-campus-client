116411d167c714b5eca0cbc0dba7ff213ed699f5
Felix <uqdgd@student.kit.edu>
2022-09-13 07:25:55 +0200

    One testing of map - not yet successful

--- src/app/modules/building/building.component.ts
+++ src/app/modules/building/building.component.ts
@@ -3,6 +3,8 @@ import {GetBuildingRequest, GetBuildingResponse, GrpcBuilding} from "../../../pr
 import {BuildingManagementConnectorService} from "../../core/connectors/building-management-connector.service";
 import {ActivatedRoute} from "@angular/router";
 
+import {Loader} from '@googlemaps/js-api-loader'
+
 @Component({
   selector: 'app-building',
   templateUrl: './building.component.html',
@@ -12,6 +14,9 @@ export class BuildingComponent implements OnInit {
 
   // path variable
   bin: string = "";
+
+
+  map!: google.maps.Map ;
 
   // main object
   building: GrpcBuilding.AsObject = new GrpcBuilding().toObject();
@@ -29,6 +34,22 @@ export class BuildingComponent implements OnInit {
     let getBuildingRequest = new GetBuildingRequest();
     getBuildingRequest.setIdentificationNumber(this.bin);
     this.buildingManagementConnector.getBuilding(getBuildingRequest, BuildingComponent.interpretGetBuildingResponse, this);
+
+
+    const loader = new Loader({
+      apiKey: 'AIzaSyDQm9qvZhW_kryxDk6UbP9vO3fsfl9dIFM',
+      version: 'weekly'
+    });
+
+    loader.load().then(() => {
+
+      this.map = new google.maps.Map(document.getElementById("map") as HTMLElement)
+
+    }  )
+
+
+
+
   }
 
   // private callback methods for api calls
