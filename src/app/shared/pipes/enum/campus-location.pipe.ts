4321d0ad305d0e1e2ab1532c6ec1778661c198d9
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-09-28 13:09:23 +0200

    übersetzung für campus locations in building table (geht nur mit "reload")

--- src/app/shared/pipes/enum/campus-location.pipe.ts
+++ src/app/shared/pipes/enum/campus-location.pipe.ts
@@ -1,20 +1,39 @@
 import {Pipe, PipeTransform} from '@angular/core';
-import {
-  GrpcCampusLocation
-} from "../../../../proto/generated/building_management_pb";
+import {GrpcCampusLocation} from "../../../../proto/generated/building_management_pb";
+import {TranslateService} from "@ngx-translate/core";
 
 @Pipe({
   name: 'campusLocation'
 })
 export class CampusLocationPipe implements PipeTransform {
 
-  transform(value: GrpcCampusLocation | string, ...args: unknown[]): unknown { //TODO übersetzen
+  translateService: TranslateService;
+
+  constructor(translateService: TranslateService) {
+    this.translateService = translateService;
+  }
+
+  transform(value: GrpcCampusLocation | string, ...args: unknown[]): unknown {
+    let key = ''
     switch (value) {
-      case GrpcCampusLocation.EAST_CAMPUS: return "East Campus"
-      case GrpcCampusLocation.NORTH_CAMPUS: return "North Campus"
-      case GrpcCampusLocation.WEST_CAMPUS: return "West Campus"
-      case GrpcCampusLocation.SOUTH_CAMPUS: return "South Campus"
-      default: return "N/A"
+      case GrpcCampusLocation.EAST_CAMPUS:
+        key = 'campus_location_pipe.east_campus'
+        break
+      case GrpcCampusLocation.NORTH_CAMPUS:
+        key = 'campus_location_pipe.north_campus'
+        break
+      case GrpcCampusLocation.WEST_CAMPUS:
+        key = 'campus_location_pipe.west_campus'
+        break
+      case GrpcCampusLocation.SOUTH_CAMPUS:
+        key = 'campus_location_pipe.south_campus'
+        break
     }
+
+    let result;
+    this.translateService.get(key).subscribe((res: string) => {
+      result = res
+    });
+    return result
   }
 }
