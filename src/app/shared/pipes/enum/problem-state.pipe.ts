ceba358a236560e2d0ce73de3aaa5803ee79dc09
Jonathan Kramer <jonathan.kramer21@gmail.com>
2022-09-28 17:13:50 +0200

    translation for pipes

--- src/app/shared/pipes/enum/problem-state.pipe.ts
+++ src/app/shared/pipes/enum/problem-state.pipe.ts
@@ -1,20 +1,43 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import {GrpcProblemState} from "../../../../proto/generated/problem_management_pb";
+import {TranslateService} from "@ngx-translate/core";
 
 @Pipe({
   name: 'problemState'
 })
 export class ProblemStatePipe implements PipeTransform {
 
+  translateService: TranslateService;
+
+  constructor(translateService: TranslateService) {
+    this.translateService = translateService;
+  }
+
   transform(value: GrpcProblemState, ...args: unknown[]): unknown {
+    let key = ''
     switch (value) {
-      case GrpcProblemState.ACCEPTED: return "Accepted"
-      case GrpcProblemState.CLOSED: return "Closed"
-      case GrpcProblemState.DECLINED: return "Declined"
-      case GrpcProblemState.IN_PROGRESS: return "In Progress"
-      case GrpcProblemState.OPEN: return "Open"
-      default: return "N/A"
+      case GrpcProblemState.ACCEPTED:
+        key = 'problem_state_type.accepted'
+        break
+      case GrpcProblemState.CLOSED:
+        key = 'problem_state_pipe.closed'
+        break
+      case GrpcProblemState.DECLINED:
+        key = 'problem_state_pipe.declined'
+        break
+      case GrpcProblemState.IN_PROGRESS:
+        key = 'problem_state_pipe.in_progress'
+        break
+      case GrpcProblemState.OPEN:
+        key = 'problem_state_pipe.open'
+        break
     }
+
+    let result;
+    this.translateService.get(key).subscribe((res: string) => {
+      result = res
+    });
+    return result
   }
 
 }
