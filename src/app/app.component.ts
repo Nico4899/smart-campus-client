45e0721ac96206b89c0337b2d059473cdcc70762
Bastian Bacher <bastianbacher@web.de>
2022-09-28 23:00:03 +0200

    cleaned up appcomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import {Component} from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
-import {MatIconRegistry} from "@angular/material/icon";
-import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-root',
@@ -11,27 +8,6 @@ import {DomSanitizer} from "@angular/platform-browser";
 export class AppComponent {
   title = 'uismartcampus';
 
-  supportLanguages = ['en', 'de'];
-
-  constructor(public translateService: TranslateService, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
-
-    // translation services
-    this.translateService.addLangs(this.supportLanguages);
-    this.translateService.setDefaultLang('en');
-    const browserLanguage = this.translateService.getBrowserLang() != undefined
-      ? this.translateService.getBrowserLang() : 'en';
-
-    //check whether language has already been set by the user before a previous reload and use it,
-    // otherwise use browser language
-    if (sessionStorage.getItem('languageChosen') != null) {
-      this.translateService.use(sessionStorage.getItem('languageChosen') as string);
-    } else {
-      // @ts-ignore
-      this.translateService.use(browserLanguage.toString());
-    }
-
-    // add custom icons
-    this.matIconRegistry.addSvgIcon('kit-header', this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/KIT_ICON_HEADER.svg"));
-    this.matIconRegistry.addSvgIcon('kit-footer', this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/KIT_ICON_FOOTER.svg"));
+  constructor() {
   }
 }
--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -1,4 +1,7 @@
 import {Component, HostBinding, OnInit} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {MatIconRegistry} from "@angular/material/icon";
+import {DomSanitizer} from "@angular/platform-browser";
 
 @Component({
   selector: 'app-default',
@@ -8,9 +11,37 @@ import {Component, HostBinding, OnInit} from '@angular/core';
 export class DefaultComponent implements OnInit {
 
   sideBarOpen = false;
-  isDark = true;
+  isDark = false;
 
-  constructor() {
+  supportLanguages = ['en', 'de'];
+
+  constructor(public translateService: TranslateService, private matIconRegistry: MatIconRegistry, private domSanitizer: DomSanitizer) {
+
+    // translation services
+    this.translateService.addLangs(this.supportLanguages);
+    this.translateService.setDefaultLang('en');
+    const browserLanguage = this.translateService.getBrowserLang() ? this.translateService.getBrowserLang() : 'en';
+
+    // add custom icons
+    this.matIconRegistry.addSvgIcon('kit-header', this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/KIT_ICON_HEADER.svg"));
+    this.matIconRegistry.addSvgIcon('kit-footer', this.domSanitizer.bypassSecurityTrustResourceUrl("../assets/images/KIT_ICON_FOOTER.svg"));
+
+    // session settings
+    let side = sessionStorage.getItem('toggleSide') as string;
+    let dark = sessionStorage.getItem('toggleDark') as string;
+    let lang = sessionStorage.getItem('languageChosen') as string;
+    if (lang) {
+      this.translateService.use(lang);
+    } else {
+      // @ts-ignore
+      this.translateService.use(browserLanguage.toString());
+    }
+    if (side) {
+      this.sideBarOpen = side != '1';
+    }
+    if (dark) {
+      this.isDark = dark != '1';
+    }
   }
 
   ngOnInit(): void {
@@ -18,10 +49,12 @@ export class DefaultComponent implements OnInit {
 
   sideBarToggler($event: any) {
     this.sideBarOpen = !this.sideBarOpen;
+    sessionStorage.setItem('toggleSide', this.sideBarOpen ? '0' : '1');
   }
 
   darkModeToggler($event: any) {
     this.isDark = !this.isDark;
+    sessionStorage.setItem('toggleDark', this.isDark ? '0' : '1');
   }
 
   @HostBinding('class')
