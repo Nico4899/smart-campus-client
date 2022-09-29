13da9f4e97aca9e7c6fdbf7a7938f933d21eb02d
Johannes von Geisau <johannes.von.geisau@t-online.de>
2022-09-29 03:07:10 +0200

    more component types

--- src/app/shared/pipes/enum/component-type.pipe.ts
+++ src/app/shared/pipes/enum/component-type.pipe.ts
@@ -24,6 +24,39 @@ export class ComponentTypePipe implements PipeTransform {
       case GrpcComponentType.STAIRS:
         key = 'component_type_pipe.stairs'
         break
+      case GrpcComponentType.DOOR:
+        key = 'component_type_pipe.door'
+        break
+      case GrpcComponentType.RAMP:
+        key = 'component_type_pipe.ramp'
+        break
+      case GrpcComponentType.CHAIR:
+        key = 'component_type_pipe.chair'
+        break
+      case GrpcComponentType.LAMP:
+        key = 'component_type_pipe.lamp'
+        break
+      case GrpcComponentType.WINDOW:
+        key = 'component_type_pipe.window'
+        break
+      case GrpcComponentType.CANVAS:
+        key = 'component_type_pipe.canvas'
+        break
+      case GrpcComponentType.WHITEBOARD:
+        key = 'component_type_pipe.whiteboard'
+        break
+      case GrpcComponentType.BEAMER:
+        key = 'component_type_pipe.beamer'
+        break
+      case GrpcComponentType.POWER_OUTLET:
+        key = 'component_type_pipe.power_outlet'
+        break
+      case GrpcComponentType.SINK:
+        key = 'component_type_pipe.sink'
+        break
+      case GrpcComponentType.HEATER:
+        key = 'component_type_pipe.heater'
+        break
     }
 
     let result;
